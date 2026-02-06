2b258e32cfe8d590164cc72f75c61275a7c3092f
ramre <ramre@VictorJRZ>
2026-02-06 11:04:46 -0600

    Inicio de proyecto ITESS-TICS POO

---
+++ src/tema1ide/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * 2026 ITESS.TICS
+ * Periodo Enero-Junio
+ *Asignatura: Programacion orientada a objetos
+ *Docente: Francisco Javier Montecillo Puente
+ *Descripción: Tema 1. Entorno de desarrollo
+ *             Prueba del entorno de desarrollo y github
+ *Intento de programador: VJRZ
+ *Fecha: 06/02/2026
+ */
+
+package tema1ide;
+
+
+public class HelloWorld {
+    public void sayHello (String name) {
+        System.out.println("Helo, " + name);
+    }
+}
