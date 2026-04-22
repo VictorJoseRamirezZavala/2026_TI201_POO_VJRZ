47083c74cd5d7318fcaec3a08fa0b33024f59820
ramre <ramre@VictorJRZ>
2026-04-22 02:38:42 -0600

    Actualice el formato de algunos codigos

--- src/tema2torteria/TortaTest.java
+++ src/tema2torteria/TortaTest.java
@@ -13,18 +13,19 @@ package tema2torteria;
  */
 
 public class TortaTest {
-   public static void main (String[]args){
+
+    public static void main(String[] args) {
         //provar el constric
-        Torta tortConstructorDefault =new Torta();
-        
-    try {
-        tortConstructorDefault.agregarIngredientes("Jamon");
-        tortConstructorDefault.agregarIngredientes("Queso");
-        tortConstructorDefault.agregarIngredientes("Lechuga");
-    } catch (Exception e) {
-        System.out.println(e.getMessage());
-    }
-            
+        Torta tortConstructorDefault = new Torta();
+
+        try {
+            tortConstructorDefault.agregarIngredientes("Jamon");
+            tortConstructorDefault.agregarIngredientes("Queso");
+            tortConstructorDefault.agregarIngredientes("Lechuga");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         System.out.println(tortConstructorDefault.toString());
-    } 
+    }
 }
